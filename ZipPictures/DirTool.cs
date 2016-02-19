fb23c485882bcadd5953cd5c08b935072a7067bd
RyoMiyazaki <roroml1@gmail.com>
2016-02-19 13:24:26 +0900

    1st check in

---
+++ ZipPictures/DirTool.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO.Compression;
+
+namespace ZipPictures
+{
+    class DirTool
+    {
+        public string[] GetAllSubDirectories(string stRootPath)
+        {
+            System.Collections.Specialized.StringCollection hStringCollection = (
+                new System.Collections.Specialized.StringCollection()
+            );
+
+            hStringCollection.Add(stRootPath);
+
+            try
+            {
+                // このディレクトリ内のすべてのサブディレクトリを検索する (再帰)
+                foreach (string stDirPath in System.IO.Directory.GetDirectories(stRootPath))
+                {
+                    string[] stFilePathes = GetAllSubDirectories(stDirPath);
+
+                    // 条件に合致したファイルがあった場合は、ArrayList に加える
+                    if (stFilePathes != null)
+                    {
+                        hStringCollection.AddRange(stFilePathes);
+                    }
+                }
+            }
+            catch (Exception)
+            {
+
+            }
+
+            // StringCollection を 1 次元の String 配列にして返す
+            string[] stReturns = new string[hStringCollection.Count];
+            hStringCollection.CopyTo(stReturns, 0);
+
+            return stReturns;
+        }
+
+        public bool IsPicturesFolder(string folder)
+        {
+            bool bRet = false;
+
+            int iCount = 0;
+            foreach (string stFilePath in System.IO.Directory.GetFiles(folder))
+            {
+                string strExt = System.IO.Path.GetExtension(stFilePath).ToLower();
+                if (strExt == ".jpg" || strExt == ".png" || strExt == ".jpeg")
+                {
+                    iCount++;
+                }
+            }
+
+            if (iCount >= 10)
+            {
+                bRet = true;
+            }
+
+            string[] subFolders = System.IO.Directory.GetDirectories(folder);
+
+            if (subFolders.Length > 0)
+            {
+                bRet = false;
+            }
+
+            return bRet;
+        }
+
+        public void createZip(string folder)
+        {
+            try
+            {
+                ZipFile.CreateFromDirectory(folder, createZipFileName(folder));
+            }
+            catch (Exception)
+            {
+
+            }
+        }
+
+        private string createZipFileName(string folder)
+        {
+            return folder + ".zip";
+        }
+
+    }
+}
---
+++ ZipPictures/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ZipPictures
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        BackgroundWorker _backgroundWorker;
+        string[] _dirs;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            _backgroundWorker = new BackgroundWorker();
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            _backgroundWorker.DoWork += new DoWorkEventHandler(Worker_DoWork);
+            _backgroundWorker.ProgressChanged += new ProgressChangedEventHandler(Worker_ProgressChanged);
+            _backgroundWorker.RunWorkerCompleted += new RunWorkerCompletedEventHandler(Worker_RunWorkerCompleted);
+            _backgroundWorker.WorkerReportsProgress = true;
+
+        }
+
+        private void btnBrowse_Click(object sender, RoutedEventArgs e)
+        {
+            System.Windows.Forms.FolderBrowserDialog fbd = new System.Windows.Forms.FolderBrowserDialog();
+            fbd.Description = "Select folder";
+            fbd.SelectedPath = @"C:\";
+            fbd.ShowNewFolderButton = false;
+            System.Windows.Forms.DialogResult dr = fbd.ShowDialog();
+            if (dr == System.Windows.Forms.DialogResult.OK)
+            {
+                //選択されたフォルダ
+                txtDirName.Text = fbd.SelectedPath;
+            }
+        }
+
+        private void btnArchive_Click(object sender, RoutedEventArgs e)
+        {
+            DirTool dir = new DirTool();
+
+
+            _dirs = dir.GetAllSubDirectories(txtDirName.Text);
+
+            int i = 0;
+            foreach (string dirName in _dirs)
+            {
+                if (dir.IsPicturesFolder(dirName))
+                {
+                    i++;
+                }
+            }
+
+
+            progBar.Minimum = 0;
+            progBar.Maximum = i;
+
+
+            this.btnArchive.IsEnabled = false;
+            this.btnBrowse.IsEnabled = false;
+            _backgroundWorker.RunWorkerAsync(this);
+
+        }
+
+        private void Worker_DoWork(object sender, DoWorkEventArgs e)
+        {
+            int i = 0;
+            DirTool dir = new DirTool();
+            foreach (string dirName in _dirs)
+            {
+                if (dir.IsPicturesFolder(dirName))
+                {
+                    dir.createZip(dirName);
+                    i++;
+                    _backgroundWorker.ReportProgress(i);
+                }
+            }
+        }
+
+
+        private void Worker_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progBar.Value = e.ProgressPercentage;
+        }
+
+
+        private void Worker_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            this.btnArchive.IsEnabled = true;
+            this.btnBrowse.IsEnabled = true;
+        }
+
+
+    }
+}
